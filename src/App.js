322b5df1b0d1448dce0bf51699fb8052e1d559f1
umnovjp <umnovjp@yahoo.com>
2022-01-06 21:28:27 -0800

    footer file added and working!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
-import Display from "./components/Display";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Card from './components/Card';
+import Footer from './components/Footer';
 
+// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
-  return <Display />;
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Card />
+      <Card />
+      <Card />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,48 @@
 import React from 'react';
 
-export default function Card(props) {
-  const cardStyle = {
-    width: '18rem',
-  };
+// We declare an object called styles that will contain a few objects for card and heading styles
+// Notice that each key lists CSS styles in camel case
+const styles = {
+  card: {
+    margin: 20,
+    background: '#e8eaf6',
+  },
+  heading: {
+    background: '#3f51b5',
+    minHeight: 50,
+    lineHeight: 3.5,
+    fontSize: '1.2rem',
+    color: 'white',
+    padding: '0 20px',
+  },
+  content: {
+    padding: 20,
+  },
+};
 
-  // Helper function that generates a random width for our placeholder images
-  const randomWidth = () => {
-    const number = Math.random() * (300 - 200) + 200;
-    return number.toString().split('.')[0];
-  };
-
+// In `Card`, we can assign a style from an object by using curly braces
+// We are assigning the card, heading, and content all from our `style` object
+function Card() {
   return (
-    <div className="container">
-      <div className="card" style={cardStyle}>
-        <img
-          className="card-img-top"
-          src={`http://placecorgi.com/${randomWidth()}`}
-          alt="Card cap"
-        />
-        <div className="card-body">
-        <h5 className="card-title">Name: {props.name}</h5>
-          <p className="card-text">Description: {props.description}</p>
-          <p className="card-text">ID: {props.id}</p>
-          <a href="#" className="btn btn-primary">
-            Adopt {props.name}
-          </a>
-        </div>
+    <div style={styles.card}>
+      <div style={styles.heading}>Lorem ipsum dolor</div>
+      <div style={styles.content}>
+        `Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+        voluptas nulla pariatur?`
       </div>
     </div>
   );
 }
+
+export default Card;
