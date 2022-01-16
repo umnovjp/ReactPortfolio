a2330552da6482ac1f3e02ef48a07725c1fccfda
umnovjp <umnovjp@yahoo.com>
2022-01-15 16:36:38 -0800

    resume link does not work

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -21,7 +21,7 @@ export default function Resume() {
       </ul>
      
 <h2>Download My Resume</h2>
-        <a href='./assets/Alexander_Github.pdf'><span className = 'book'>🕮</span></a>
+        <a href='../assets/Alexander_Github.pdf'><span className = 'book'>🕮</span></a>
         <br></br>
         <br></br>
     </div>
